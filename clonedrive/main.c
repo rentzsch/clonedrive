4c829bd24fa0624bde4ab5e2cddf569a8afabea7
rentzsch <jwr.git@redshed.net>
2013-05-05 14:49:21 -0500

    Initial Commit

---
+++ clonedrive/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  clonedrive
+//
+//  Created by Wolf Rentzsch on 5/5/13.
+//  Copyright (c) 2013 Jonathan 'Wolf' Rentzsch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
